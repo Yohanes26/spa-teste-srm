9416b400b5aefb34daedc4d0eb038df615cc346d
Yohanes26 <yohaneslopes@gmail.com>
2018-11-16 04:20:16 -0200

    Ajustando header de requisicao

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var path = require('path');
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/spa-teste-srm'));
-
+app.use(function (req, res, next) {
+    req.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
